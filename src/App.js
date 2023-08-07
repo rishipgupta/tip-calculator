9b80e22716a734f313551d41da273ab677f57914
rishipgupta <guptariship@gmail.com>
2023-08-07 19:20:38 +0530

    Create reset button to make values default on click

--- src/App.js
+++ src/App.js
@@ -17,34 +17,44 @@ function App() {
     setTipTwo(tipTwo => e.target.value);
   }
 
+  const handleReset = (e) => {
+    // e.preventDefault();
+    setBill("");
+    setTipOne(0);
+    setTipTwo(0);
+  }
+
   const tipAvg = Math.round(((Number(tipOne) + Number(tipTwo)) / 200) * Number(bill));
 
   return (
     <div>
       <h1>Tip Calculator</h1>
-      <div>
-        <span>How much was the bill? </span> <input type="text" onChange={handleBillAmountChange} />
-      </div>
-      <div>
-        <span>How did you like the service</span><select onChange={handleTipOneChange}>
-          <option value={0}>Dissatisfied (0%)</option>
-          <option value={5}>It was okay (5%)</option>
-          <option value={10}>It was good (10%)</option>
-          <option value={20}>Absolutely amazing (20%)</option>
-        </select>
-      </div>
-      <div>
-        <span>How did you like the service</span><select onChange={handleTipTwoChange}>
-          <option value={0}>Dissatisfied (0%)</option>
-          <option value={5}>It was okay (5%)</option>
-          <option value={10}>It was good (10%)</option>
-          <option value={20}>Absolutely amazing (20%)</option>
-        </select>
-      </div>
-      <div>
-        {/* Display */}
-        <h1>{`You pay $${Number(bill) + Number(tipAvg) ? Number(bill) + Number(tipAvg) : 0} (${bill ? bill : 0} + ${tipAvg})`}</h1>
-      </div>
+      <form onSubmit={handleReset}>
+        <div>
+          <span>How much was the bill? </span> <input type="text" onChange={handleBillAmountChange} />
+        </div>
+        <div>
+          <span>How did you like the service</span><select onChange={handleTipOneChange}>
+            <option value={0}>Dissatisfied (0%)</option>
+            <option value={5}>It was okay (5%)</option>
+            <option value={10}>It was good (10%)</option>
+            <option value={20}>Absolutely amazing (20%)</option>
+          </select>
+        </div>
+        <div>
+          <span>How did you like the service</span><select onChange={handleTipTwoChange}>
+            <option value={0}>Dissatisfied (0%)</option>
+            <option value={5}>It was okay (5%)</option>
+            <option value={10}>It was good (10%)</option>
+            <option value={20}>Absolutely amazing (20%)</option>
+          </select>
+        </div>
+        <div>
+          {/* Display */}
+          <h1>{`You pay $${Number(bill) + Number(tipAvg) ? Number(bill) + Number(tipAvg) : 0} (${bill ? bill : 0} + ${tipAvg})`}</h1>
+        </div>
+        <button onClick={handleReset}>Reset</button>
+      </form>
     </div>
   );
 }
