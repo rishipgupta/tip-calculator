6b0fd2ac746735d756f081a05caad02ef26b0266
rishipgupta <guptariship@gmail.com>
2023-08-07 18:58:19 +0530

    Capturing values of first select component

--- src/App.js
+++ src/App.js
@@ -2,9 +2,14 @@ import { useState } from "react";
 
 function App() {
   const [bill, setBill] = useState(0);
+  const [tipOne, setTipOne] = useState(0)
 
   const handleBillAmountChange = (e) => {
     setBill(bill => e.target.value);
+  }
+
+  const handleTipOneChange = (e) => {
+    setTipOne(tipOne => e.target.value);
   }
 
   return (
@@ -14,7 +19,7 @@ function App() {
         <span>How much was the bill? </span> <input type="text" onChange={handleBillAmountChange} />
       </div>
       <div>
-        <span>How did you like the service</span><select>
+        <span>How did you like the service</span><select onChange={handleTipOneChange}>
           <option value={0}>Dissatisfied (0%)</option>
           <option value={5}>It was okay (5%)</option>
           <option value={10}>It was good (10%)</option>
@@ -23,7 +28,7 @@ function App() {
       </div>
       <div>
         {/* Display */}
-        <h1>You pay ${bill ? bill : 0}</h1>
+        <h1>{`You pay $${bill ? bill : 0} (${bill} + ${Math.round((tipOne * bill) / 100)})`}</h1>
       </div>
     </div>
   );
