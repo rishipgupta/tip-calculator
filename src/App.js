8a60d269b30bce1c89ed12a548beffcb84e8c7e7
rishipgupta <guptariship@gmail.com>
2023-08-07 18:43:50 +0530

    Create input tag and managed its value using useState

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import { useState } from "react";
 
 function App() {
+  const [bill, setBill] = useState(0);
+
+  const handleBillAmountChange = (e) => {
+    setBill(bill => e.target.value);
+  }
+
   return (
     <div>
       <h1>Tip Calculator</h1>
+      <div>
+        <span>How much was the bill? </span> <input type="text" onChange={handleBillAmountChange} />
+      </div>
+      <div>
+        {/* Display */}
+        <h1>You pay ${bill ? bill : 0}</h1>
+      </div>
     </div>
   );
 }
